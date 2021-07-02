d051a0d0ee3545ef47fd6b83facf0ac55721a156
Sarah Porzig <sarahporzig@icloud.com>
2021-07-02 21:29:18 +1000

    major changes to pportfolio site including images layout and text

--- js/script.js
+++ js/script.js
@@ -2,14 +2,10 @@ $(document).ready(function() {
 
   $(".myProjectsButton").click(function() {
     $('html, body').animate({
-      scrollTop: $("#myProjects").offset().top - 100
+      scrollTop: $("#myProjects").offset().top - 0
     }, 400);
   });
-  $(".aboutMeButton").click(function() {
-    $('html, body').animate({
-      scrollTop: $("#aboutMe").offset().top - 100
-    }, 400);
-  });
+  
   $('.nav a').on('click', function(){
       $('.navbar-toggle').click()
   });
